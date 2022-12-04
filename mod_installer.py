61d9da8430c89c3785687d573293081db1d6d224
equdevel <bav@tuta.io>
2022-12-05 00:57:45 +0800

    update user_profile.cfg

--- mod_installer.py
+++ mod_installer.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 import shutil
 from pprint import pprint, pformat
@@ -9,6 +10,8 @@ from tqdm import tqdm
 
 load_dotenv()
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+USER_PROFILE = os.getenv('USER_PROFILE')
+MODS_DIR = os.getenv('MODS_DIR')
 
 headers = {
     'Authorization': f'Bearer {ACCESS_TOKEN}',
@@ -21,30 +24,39 @@ data = {
 }
 
 r = requests.get('https://api.mod.io/v1/me/subscribed', params={}, headers=headers, json=data)
-
 # pprint(r.json())
+
+with open(USER_PROFILE, mode='r', encoding='utf-8') as f:
+    user_profile = json.load(f)
+# pprint(user_profile)
 
 for data in r.json()['data']:
     mod_id = data['id']
     mod_name = data['name']
+    mod_dir = f'{MODS_DIR}/{mod_id}'
     try:
-        os.mkdir(str(mod_id))
+        os.mkdir(mod_dir)
     except FileExistsError:
         pass
     else:
-        with open(f'{mod_id}/modio.json', mode='w', encoding='utf-8') as f:
-            f.write(json.dumps(data, ensure_ascii=False, indent=4))
+        with open(f'{MODS_DIR}/{mod_id}/modio.json', mode='w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
         mod_url = data['modfile']['download']['binary_url']
         mod_filename = data['modfile']['filename']
-        mod_path = f'{mod_id}/{mod_filename}'
+        mod_fullpath = f'{MODS_DIR}/{mod_id}/{mod_filename}'
         print(f'Downloading "{mod_name}" ({mod_filename})')
         response = requests.get(mod_url, stream=True)
-        with open(mod_path, mode='wb') as handle:
+        with open(mod_fullpath, mode='wb') as f:
             for chunk in tqdm(response.iter_content(chunk_size=1024**2), unit=' Mb'):
-                handle.write(chunk)
+                f.write(chunk)
         print(f'Unpacking {mod_filename}...', end='')
-        shutil.unpack_archive(mod_path, str(mod_id), 'zip')
-        os.remove(mod_path)
+        shutil.unpack_archive(mod_fullpath, mod_dir, 'zip')
+        os.remove(mod_fullpath)
         print('OK')
+        user_profile['UserProfile']['modDependencies']['SslValue']['dependencies'].update({str(mod_id): []})
+        # user_profile['UserProfile']['modStateList'].append({'modId': mod_id, 'modState': False})
     finally:
         pass
+
+with open(USER_PROFILE, mode='w', encoding='utf-8') as f:
+    json.dump(user_profile, f, ensure_ascii=False, indent=4)
